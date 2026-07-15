4555230dcb451608f269020716f2313d50330688
Joe Daniel A <joedanielajd@gmail.com>
2026-07-15 19:15:05 +0530

    added pattern 6

---
+++ Patterns/Pattern6/pattern6.c
@@ -0,0 +1,19 @@
+
+#include <stdio.h>
+void pattern1(int N)
+{
+    for(int i = 1; i < N; i++)
+    {
+        for(int j = 1; j <= N-i+1; j++)
+        {
+            printf("%d ", j);
+        }
+        printf("\n");
+    }
+}
+int main()
+{
+    int N = 5;
+    pattern1(N);
+    return 0;
+}
