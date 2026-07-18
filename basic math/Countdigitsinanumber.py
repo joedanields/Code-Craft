fec0c53387d680a7e2c0c39960c2d1acaabd4f05
Joe Daniel A <joedanielajd@gmail.com>
2026-07-18 13:42:12 +0530

    Create Countdigitsinanumber.py

---
+++ basic math/Countdigitsinanumber.py
@@ -0,0 +1,15 @@
+                     
+import math
+.
+def countDigits(n):
+    cnt = int(math.log10(n) + 1)
+    return cnt
+
+# Main function
+if __name__ == "__main__":
+    N = 329823
+    print("N:", N)
+    digits = countDigits(N)
+    print("Number of Digits in N:", digits)
+
+                         
